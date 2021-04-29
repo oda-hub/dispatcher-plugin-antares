6cf27c3ade4d366af7fb061e5d5b69038a159e1a
Denys Savchenko <speleoden@gmail.com>
2021-04-29 17:48:31 +0200

    test product download

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,10 +1,9 @@
 import requests
-import time
 import json
 import logging
 
 import pytest
-
+from astropy.io import fits
 
 logger = logging.getLogger(__name__)
 
@@ -64,3 +63,35 @@ def test_default(dispatcher_live_fixture):
     assert c.status_code == 200
 
     assert jdata['job_status'] == 'done'
+
+@pytest.mark.depends(on=['test_default'])
+def test_file_download(dispatcher_live_fixture):
+    server = dispatcher_live_fixture
+
+    logger.info("constructed server: %s", server)
+    c = requests.get(server + "/run_analysis",
+                     params = default_params)
+
+    logger.info("content: %s", c.text)
+    jdata = c.json()
+    logger.info(json.dumps(jdata, indent=4, sort_keys=True))
+    logger.info(jdata)
+    
+    d = requests.get(server + "/download_products",
+                    params = {
+                        'session_id': jdata['job_monitor']['session_id'],
+                        'download_file_name': jdata['products']['download_file_name'],
+                        'file_list': jdata['products']['file_name'][0],
+                        'query_status': 'ready',
+                        'job_id': jdata['job_monitor']['job_id'],
+                        'instrument': 'antares'
+                    })
+    assert d.status_code == 200
+
+    with open('tmp.fits.gz', 'wb') as fd:
+        fd.write(d.content)
+
+    with fits.open('tmp.fits.gz', 'readonly') as fd:
+        for ext in fd:
+            logger.info(ext.header)
+            logger.info(ext.data)
