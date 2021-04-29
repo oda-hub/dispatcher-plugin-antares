da555baf883322b3d86f9316fa7143e6a7cf7740
Denys Savchenko <speleoden@gmail.com>
2021-04-29 16:30:23 +0200

    req. 4 actions to not interfere container

--- setup.py
+++ setup.py
@@ -13,13 +13,20 @@ __author__ = 'andrea tramacere'
 
 from setuptools import setup, find_packages
 import  glob
-
+import re
 
 
 f = open("./requirements.txt",'r')
 install_req=f.readlines()
 f.close()
 
+def reformat_git_req(req_str):
+    match = re.match('.*(git\+.+egg=(.+)$)', req_str)
+    if match:
+        req_str = "%s @ %s"%(match.group(2), match.group(1))
+    return req_str
+
+install_req = [reformat_git_req(x) for x in install_req]
 
 packs=find_packages()
 
